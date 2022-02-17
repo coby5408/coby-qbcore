08982cd84bf7b935a5c682a41207d346283d310c
Coby <mascaradopsn@gmail.com>
2022-02-17 20:30:18 +0000

    Template added

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Coby-Coke'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+lua54 'yes'
